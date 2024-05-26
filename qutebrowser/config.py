4f0daef6a223dc1791254a7959a46da22bfab2d0
Farid <faridfaracli9@gmail.com>
2024-05-27 01:06:37 +0400

    Just a better qutebrowser config

---
+++ qutebrowser/config.py
@@ -0,0 +1,8 @@
+c.url.default_page = 'https://searx.hu'
+c.url.start_pages  = 'https://searx.hu'
+c.fonts.default_size = '14pt'
+c.url.searchengines = {
+    'DEFAULT':  'https://searx.hu/search?q={}',
+}
+
+config.load_autoconfig()
