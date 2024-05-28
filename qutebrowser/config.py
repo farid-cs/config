bd28b5e138167f89394a69d166ca77063160c97e
Farid <faridfaracli9@gmail.com>
2024-05-28 23:20:48 +0400

    Configuration in qutebrowser made more readable

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,7 +1,7 @@
-c.url.default_page = 'https://searx.hu'
-c.url.start_pages  = 'https://searx.hu'
+c.url.default_page   = 'https://searx.hu'
+c.url.start_pages    = 'https://searx.hu'
 c.fonts.default_size = '14pt'
-c.url.searchengines = {
+c.url.searchengines  = {
     'DEFAULT':  'https://searx.hu/search?q={}',
 }
 
