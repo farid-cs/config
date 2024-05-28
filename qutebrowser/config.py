c5776fa0b17933059b6e3447d8fab90bc0ac6013
Farid <faridfaracli9@gmail.com>
2024-05-29 03:27:26 +0400

    Files downloaded from qutebrowser are now placed in ~/downloads instead of ~/Downloads

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -4,5 +4,6 @@ c.fonts.default_size = '14pt'
 c.url.searchengines  = {
     'DEFAULT':  'https://searx.hu/search?q={}',
 }
+c.downloads.location.directory = '~/downloads'
 
 config.load_autoconfig()
