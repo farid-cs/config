1ae43fd4e6912ea9a92c8da8da6fec49d4ca1f42
Farid <faridfaracli9@gmail.com>
2024-05-29 10:24:16 +0400

    NvimTree is now toggled instead of focusing on <leader>e keymap

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeFocus<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>w', '<C-w>',                  { noremap = true, silent = true })
