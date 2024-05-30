f19ff8bc79c7dc9fee67bb2c1da2ee64eefa5651
Farid <faridfaracli9@gmail.com>
2024-05-30 17:53:39 +0400

    Default search engine set to google for qutebrowser because it works

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,8 +1,8 @@
-c.url.default_page   = 'https://searx.hu'
-c.url.start_pages    = 'https://searx.hu'
+c.url.default_page   = 'https://google.com'
+c.url.start_pages    = 'https://google.com'
 c.fonts.default_size = '14pt'
 c.url.searchengines  = {
-    'DEFAULT':  'https://searx.hu/search?q={}',
+    'DEFAULT':  'https://www.google.com/search?q={}',
 }
 c.downloads.location.directory = '~/downloads'
 
