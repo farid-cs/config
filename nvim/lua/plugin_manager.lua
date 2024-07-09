f7d91a43a5b034d0a2f2cdf4002017bd53b27450
Farid <faridfaracli9@gmail.com>
2024-07-09 17:52:32 +0400

    Lsp added, plus colorscheme setting moved to lua/plugin_manager.lua

--- nvim/lua/plugin_manager.lua
+++ nvim/lua/plugin_manager.lua
@@ -9,15 +9,25 @@ end
 
 require('lazy').setup({
 	{
+		'neovim/nvim-lspconfig',
+		init = function()
+			require'lspconfig'.clangd.setup{}
+		end,
+	},
+	{
 		'windwp/nvim-autopairs',
 		config = true,
 	},
 	{
 		'ellisonleao/gruvbox.nvim',
 		opts = {
-			contrast = 'hard',
-			italic   = {strings = false},
+			contrast  = 'hard',
+			italic    = {strings = false},
+			bold      = false,
 		},
+		init = function()
+			vim.cmd.colorscheme 'gruvbox'
+		end,
 	},
 	{
 		'akinsho/toggleterm.nvim',
