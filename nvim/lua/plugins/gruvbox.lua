098790be2baa77c2cc4d41b9530030c1f1b0ee8b
Farid <faridfaracli9@gmail.com>
2024-07-26 20:15:14 +0400

    No bold functions and methods anymore

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -3,7 +3,12 @@ return {
 	opts = {
 		contrast  = 'hard',
 		italic    = {strings = false},
-		bold = false,
+		overrides = {
+			['@function'] = {fg = '#b8bb26'},
+			['@function.call'] = {fg = '#b8bb26'},
+			['@function.method'] = {fg = '#b8bb26'},
+			['@function.method.call'] = {fg = '#b8bb26'},
+		},
 	},
 	init = function()
 		vim.cmd.colorscheme 'gruvbox'
