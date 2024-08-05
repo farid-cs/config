425f4b28081161cd1bea97af6bd12df48a192149
Farid <faridfaracli9@gmail.com>
2024-08-05 14:10:16 +0400

    Run the function when lsp is loaded not by default

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,7 +1,9 @@
 return {
 	'neovim/nvim-lspconfig',
-	init = function()
+	config = function()
 		require'lspconfig'.clangd.setup{}
 		require'lspconfig'.gopls.setup{}
+		require'lspconfig'.rust_analyzer.setup{}
 	end,
+	cmd = 'LspStart',
 }
