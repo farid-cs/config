7d43c42b685ddecc158f6fe57095c488d1f7a781
Farid <faridfaracli9@gmail.com>
2024-09-15 00:53:36 +0400

    Don't load autoconfig.yml and don't set downloads directory in qutebrowser also set default zoom level and enable dark mode.

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -4,6 +4,6 @@ c.fonts.default_size = '14pt'
 c.url.searchengines  = {
     'DEFAULT':  'https://www.google.com/search?hl=en&q={}',
 }
-c.downloads.location.directory = '~/downloads'
-
-config.load_autoconfig()
+c.zoom.default       = '150%'
+c.colors.webpage.darkmode.enabled = True
+config.load_autoconfig(False)
