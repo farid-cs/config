d93c418e61b12c71fb5619eddc1fcb635a7677e7
Farid <faridfaracli9@gmail.com>
2024-09-28 18:53:48 +0400

    Open toggleterm in less key strokes

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,12 +1,12 @@
 return {
 	'akinsho/toggleterm.nvim',
 	opts = {
-		terminal_mappings = true,
+		terminal_mappings = false,
 		insert_mappings   = false,
-		open_mapping      = '<c-t>',
+		open_mapping      = '<leader>t',
 		float_opts        = {height = 40, width = 160},
 		direction         = 'float',
 		autochdir         = true,
 	},
-	keys = '<C-t>',
+	cmd = 'ToggleTerm',
 }
