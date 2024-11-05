0cdf5996d1c3869c7936bc1f3e9d77af8bceb5ed
Farid <faridfaracli9@gmail.com>
2024-11-05 13:49:06 +0400

    brave search instead of google

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,8 +1,8 @@
-c.url.default_page   = 'https://google.com'
-c.url.start_pages    = 'https://google.com'
+c.url.default_page   = 'https://search.brave.com'
+c.url.start_pages    = 'https://search.brave.com'
 c.fonts.default_size = '14pt'
 c.url.searchengines  = {
-    'DEFAULT':  'https://www.google.com/search?hl=en&q={}',
+    'DEFAULT':  'https://search.brave.com/search?q={}',
 }
 c.zoom.default       = '150%'
 c.colors.webpage.darkmode.enabled = True
